5d4faaab9daca3863832c6f0625f298771b390d3
colivarese <ces.oe97@gmail.com>
2021-10-26 15:39:26 -0500

    WORKING

---
+++ MLAR/fun.py
@@ -0,0 +1,69 @@
+import numpy as np
+import itertools
+
+def readTxt(dataFile):
+    with open(dataFile, "r") as filestream:
+        table = []
+        for line in filestream:
+            currentline = line.strip("\n").split(",")
+            table.append(currentline)
+    return table
+
+def get_large_1_itemsets(trans_table:list, l:int) -> dict: 
+    items = []
+    for row in trans_table:
+        for item in row:
+            if item[0:l] not in items:
+                items.append(item[0:l])
+    items = {k:0 for k in items}
+    for i, row in enumerate(trans_table):
+        items_in_row = []
+        for item in row:
+            items_in_row.append(str(item[0:l]))
+        items_keys = list(items.keys())
+        for item in items_keys:
+            if item in items_in_row:
+                items[item] += 1
+    return items
+
+def get_filtered_t_table(trans_table:list, L:dict, l:int, minSup:list) -> list:
+    minSup = minSup[l-1]
+    supported_items = [k for k in L.keys() if L[k] >= minSup]
+    filteredTable = []
+    for i,row in enumerate(trans_table):
+        items_in_row = []
+        for item in row:
+            c = (str(item[0:l]))
+            if inSet(supported_items, c):
+                items_in_row.append(item)
+        if items_in_row:
+            filteredTable.append(items_in_row)
+    return filteredTable
+
+def get_candidate_set(L):
+    return list(itertools.combinations(L.keys(),2))
+
+def get_subset_support(Ck, table, minSup, l):
+    subsets = {k:0 for k in Ck}
+    for row in table:
+        items_in_row = []
+        for r in row:
+            items_in_row.append(r[0:l])
+        for c in Ck:
+            if inSet(items_in_row,c):
+                subsets[c] += 1
+    return subsets
+
+def inSet(inRow, items):
+    if set(items).issubset(inRow):
+        return True
+    return False
+
+def printLL(LL):
+    print(" ---- "*3)
+    for k,v in LL.items():
+        if v:
+            print("    L[",k,"]   =>",'\n')
+            print(v)
+            print("----\n"*2)
+    
--- MLAR/main.py
+++ MLAR/main.py
@@ -2,44 +2,27 @@ import csv
 from treelib import Node, Tree
 from utils import *
 import itertools
+from fun import *
 
 if __name__ == '__main__':
     table = readTxt('./MLAR/transactionTable.txt')
     items = getItemSets(table=table)
 
-    L =  {}
+    LL = {}
+    minSup = [4,3,3]
+    minSup.append(minSup[-1])
     for l in range(1,4):
         if l==1:
-            printTable(table)
-            items = searchItem(table, items,l,minSup=5)
-
-            table, minSup = filterTable(table, items, minSup=5, size=l)
-            printTable(table)
+            LL[(l,1)] = get_large_1_itemsets(trans_table=table, l=l)
+            table = get_filtered_t_table(trans_table=table, L=LL[(l,1)], l=l, minSup=minSup)
         else:
-            if l > 2 :
-                items = getCombinations_2(items)
-                items = CombineKeys(items, l)
+            LL[(l,1)] = get_large_1_itemsets(trans_table=table, l=l)
+        for k in range(2,4):
+            candidate_set = get_candidate_set(LL[(l,k-1)])
+            subsets = get_subset_support(Ck = candidate_set, table=table, minSup=minSup[l], l=l)
+            LL[(l,k)] = {k:v for k,v in subsets.items() if v >= minSup[l]}
 
-                items = searchItem_2(table, items, 2, 3)
-                items = getCombinations_2(items)
-                items = CombineKeys(items, 3)
-                #items = CombineKeys(items,l)
+    printLL(LL)
 
-                table, minSup = filterTable_3(table, items, 3, size=2)
-                printTable(table)
-                items  = searchItem_2(table, items, l, 3)
-
-
-            else:
-                items = getCombinations(items)
-
-                items = searchItem(table, items, l, minSup)
-                print("")
-                items = CombineKeys(items,l)
-
-                table, minSup = filterTable_2(table, items, 3, size=l)
-                printTable(table)
-                items = searchItem_2(table, items, l, 3)
-                pass
 
             
